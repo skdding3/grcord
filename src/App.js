7144f98f73feb189a0d93d4de0aa338499c1b020
skdding3 <skdding3@gmail.com>
2022-06-19 17:36:24 +0900

    [feat] userStore, Main page add

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import { Routes, Route } from "react-router-dom";
 import Join from "./pages/Join";
 import "./App.css";
 import Login from "./pages/Login";
+import { useDispatch } from "react";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { clearUser, setUser } from "./store/userReducer";
 
 function App() {
+  const dispatch = useDispatch();
+  const { isLoading, currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+      if (!!user) {
+        dispatch(setUser(user));
+      } else {
+        dispatch(clearUser());
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
   return (
     <Routes>
+      <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/join" element={<Join />} />
     </Routes>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore } from "redux";
+import rootReducer from "./store";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={legacy_createStore(rootReducer)}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
