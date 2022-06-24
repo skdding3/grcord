1306ef6b531e984f38e0d5b5be00535ff16a5d94
skdding3 <skdding3@gmail.com>
2022-06-24 17:51:02 +0900

    [feat] Realtime chat 구현

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,12 +1,54 @@
 import { Divider, Grid, List, Paper, Toolbar } from "@mui/material";
-import React from "react";
+import {
+  child,
+  ref,
+  getDatabase,
+  get,
+  orderByChild,
+  query,
+  onChildAdded,
+  startAt,
+} from "firebase/database";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import ChatHeader from "./ChatHeader";
 import Chatinput from "./Chatinput";
 import ChatMessage from "./ChatMessage";
 
 function Chat() {
-  const { channel } = useSelector((state) => state);
+  const { channel, user } = useSelector((state) => state);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (!channel.currentChannel) return;
+    async function getMessages() {
+      const snapShot = await get(
+        child(ref(getDatabase()), "messages/" + channel.currentChannel.id)
+      );
+      setMessages(snapShot.val() ? Object.values(snapShot.val()) : []);
+    }
+    getMessages();
+    return () => {
+      setMessages([]);
+    };
+  }, [channel.currentChannel]);
+
+  useEffect(() => {
+    if (!channel.currentChannel) return;
+    const sorted = query(
+      ref(getDatabase(), "messages/" + channel.currentChannel.id),
+      orderByChild("timestamp")
+    );
+    const unsubscribe = onChildAdded(
+      query(sorted, startAt(Date.now())),
+      (snapshot) =>
+        setMessages((oldMessages) => [...oldMessages, snapshot.val()])
+    );
+    return () => {
+      unsubscribe?.();
+    };
+  }, [channel.currentChannel]);
   return (
     <>
       <Toolbar />
@@ -26,7 +68,13 @@ function Chat() {
           }}
         >
           {/* 채팅메세지 */}
-          <ChatMessage />
+          {messages.map((message) => (
+            <ChatMessage
+              key={message.timestamp}
+              message={message}
+              user={user}
+            />
+          ))}
         </List>
         <Divider />
         <Chatinput />
--- src/components/Chat/ChatMessage.jsx
+++ src/components/Chat/ChatMessage.jsx
@@ -5,9 +5,13 @@ import {
   ListItemAvatar,
   ListItemText,
 } from "@mui/material";
+import dayjs from "dayjs";
 import React from "react";
 
-function ChatMessage() {
+const relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
+
+function ChatMessage({ message, user }) {
   return (
     <ListItem>
       <ListItemAvatar sx={{ alignself: "stretch" }}>
@@ -15,15 +19,21 @@ function ChatMessage() {
           variant="rounded"
           sx={{ width: 50, height: 50 }}
           alt="profile image"
+          src={message.user.avatar}
         />
       </ListItemAvatar>
       <Grid container sx={{ ml: 2 }}>
         <Grid item xs={12} sx={{ display: "flex", justifyContent: "left" }}>
           <ListItemText
             sx={{ display: "flex" }}
-            primary={"닉네임"}
-            primaryTypographyProps={{ fontWeight: "bold", color: "orange" }}
-            secondary={"2022.06.24"}
+            primary={message.user.name}
+            primaryTypographyProps={{
+              fontWeight: "bold",
+              // 본인인 경우 오렌지 아니면 블랙이도록 채팅메세지 컬러설정
+              color:
+                message.user.id === user.currentUser.uid ? "orange" : "black",
+            }}
+            secondary={dayjs(message.timestamp).fromNow()}
             secondaryTypographyProps={{ color: "gray", ml: 1 }}
           />
         </Grid>
@@ -31,7 +41,7 @@ function ChatMessage() {
           <ListItemText
             align="left"
             xs={{ wordBreak: "break-all" }}
-            primary={"채팅 메세지 입니다."}
+            primary={message.content}
           />
           {/* TODO 이미지 추가 */}
           {/* <img alt="message" src="" style={{maxWidth:"100%"}} /> */}
