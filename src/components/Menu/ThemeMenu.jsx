a5555c43fbf3c77dbe04fde2b00b126bfccaf23f
skdding3 <skdding3@gmail.com>
2022-07-03 17:28:22 +0900

    [feat] theme stack

--- src/components/Menu/ThemeMenu.jsx
+++ src/components/Menu/ThemeMenu.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { List, ListItem, ListItemIcon } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemIcon,
+  Stack,
+} from "@mui/material";
 import PaletteIcon from "@mui/icons-material/Palette";
+import { useState, useCallback } from "react";
+
+const [showThemeModal, setShowThemeModal] = useState(false);
+
+const handleClickOpen = useCallback(() => setShowThemeModal(true), []);
+const handleClose = useCallback(() => setShowThemeModal(false), []);
 
 function ThemeMenu() {
   return (
     <>
       <List sx={{ overflow: "auto", width: 60, backgroundColor: "#150C16" }}>
-        <ListItem button>
+        <ListItem button onClick={handleClickOpen}>
           <ListItemIcon sx={{ color: "white" }}>
             <PaletteIcon />
           </ListItemIcon>
@@ -25,6 +39,15 @@ function ThemeMenu() {
           </div>
         </ListItem>
       </List>
+      <Dialog open={showThemeModal} onClose={handleClose}>
+        <DialogTitle>테마 색상 선택</DialogTitle>
+        <DialogContent>
+          <Stack direction="row" spacing={2}>
+            <div>Main</div>
+            <div>Sub</div>
+          </Stack>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
